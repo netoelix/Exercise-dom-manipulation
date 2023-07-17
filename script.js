6a2b637227bac18317726b827a899d8ad201f623
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-16 21:25:30 -0300

    Mudando letra do primeiro paragrafo para maiuscula

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementsByTagName('p')[1].textContent = 'Em dois anos eu me vejo tra
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76, 164, 109)';
 document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
 document.querySelector('h1').textContent = 'Desafio - JavaScript';
+
+let upperCase = document.getElementsByTagName('p')[0].innerHTML;
+let newUpperCase = upperCase.toUpperCase();
+document.getElementsByTagName('p')[0].textContent = newUpperCase;

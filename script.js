c23b8d6e248e6c260b4c19159d76915a8c413f76
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-16 21:16:13 -0300

    Mudando a cor do main-content de amarelo para a nova cor rgb

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 // Seu código aqui inicio
 document.getElementsByTagName('p')[1].textContent = 'Em dois anos eu me vejo trabalhando como pessoa desenvolvedroa full stack';
+document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76, 164, 109)';

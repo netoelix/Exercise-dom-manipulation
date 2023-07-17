fa9687a2d047814531769f0ebbee2b7f485bc208
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-16 21:17:55 -0300

    Mudando cor de fundo vermelha para branca

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // Seu código aqui inicio
 document.getElementsByTagName('p')[1].textContent = 'Em dois anos eu me vejo trabalhando como pessoa desenvolvedroa full stack';
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76, 164, 109)';
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
